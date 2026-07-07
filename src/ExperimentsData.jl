bea51a97f8d46ca14cf18d59ff4312663ee748b0
Miguel Masó <miguel.maso@upc.edu>
2026-07-07 15:06:33 +0200

    initial commit

---
+++ src/ExperimentsData.jl
@@ -0,0 +1,82 @@
+module ExperimentsData
+
+abstract type AbstractMeasurement end
+abstract type AbstractProtocol end
+abstract type AbstractCondition end
+
+# --- Kinematics ---
+
+abstract type Kinematics end
+struct Uniaxial <: Kinematics end
+struct Biaxial <: Kinematics end
+
+# --- Measurements ---
+
+struct TensileMeasurement <: AbstractMeasurement
+  σ::Vector{Float64}
+end
+
+struct ThermalMeasurement <: AbstractMeasurement
+  cv::Vector{Float64}
+end
+
+struct DielectricMeasurement <: AbstractMeasurement
+  ε::Vector{Float64}
+end
+
+# --- Protocols ---
+
+struct SequentialProtocol <: AbstractProtocol
+  stages::Vector{AbstractProtocol}
+end
+
+struct QuasiStaticProtocol{K<:Kinematics} <: AbstractProtocol
+  λ::Vector{Float64}
+end
+
+struct CyclicLoadingProtocol{K<:Kinematics} <: AbstractProtocol
+  v::Float64
+  Δt::Float64
+  λ::Vector{Float64}
+end
+
+struct CreepProtocol{K<:Kinematics} <: AbstractProtocol
+  t::Vector{Float64}
+end
+
+struct TemperatureSweepProtocol <: AbstractProtocol
+  θ::Vector{Float64}
+end
+
+struct FrequencySweepProtocol <: AbstractProtocol
+  f::Vector{Float64}
+end
+
+# --- Conditions ---
+
+struct StandardCondition <: AbstractCondition
+end
+
+struct IsothermalCondition <: AbstractCondition
+  θ::Float64
+end
+
+struct ElectricalCondition <: AbstractCondition
+  V::Float64
+end
+
+struct ThermoElectricalCondition <: AbstractCondition
+  θ::Float64
+  V::Float64
+end
+
+# --- Experiments ---
+
+mutable struct ExperimentData{M<:AbstractMeasurement, P<:AbstractProtocol, C<:AbstractCondition}
+  const measurement::M
+  const protocol::P
+  const condition::C
+  weight::Float64
+end
+
+end
---
+++ src/HyperCalibration.jl
@@ -0,0 +1,7 @@
+module HyperCalibration
+
+include("ExperimentsData.jl")
+include("ConstitutiveModelling.jl")
+include("Exports.jl")
+
+end
