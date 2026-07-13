155827a888b4a07f875994104ec9211e0c5c396a
Miguel Masó <miguel.maso@upc.edu>
2026-07-13 12:55:42 +0200

    Upgrade HyperFEM, reomve Gridap dep

--- src/HyperCalibration.jl
+++ src/HyperCalibration.jl
@@ -1,10 +1,7 @@
 module HyperCalibration
 
 using HyperFEM
-using Gridap.TensorValues
 using Printf
-
-import HyperFEM.PhysicalModels:PhysicalModel, Elasto, ViscoElastic
 
 include("ExperimentsData.jl")
 include("ExperimentsInterface.jl")
